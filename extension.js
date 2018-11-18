2b9eb45809e9249094f1c660bcdc87ef00050ea1
Margherita Lazzarini <margherita.lazzarini@gmail.com>
2018-11-18 11:53:14 +0100

    Toggle pause and resume
    plus removed commented/debug code

--- extension.js
+++ extension.js
@@ -1,17 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let isOver;
+let isPaused = false;
+let timer;
+let statusBarItem;
+let value = 30;
+
+function startTimer(value, barItem) {
+	const duration = 60 * value;
+	let currentTime = duration;
+	let minutes, seconds;
+	if (timer) clearInterval(timer);
+	timer = setInterval(() => {
+		if (!isPaused) {
+			minutes = parseInt(currentTime / 60, 10)
+			seconds = parseInt(currentTime % 60, 10);
+	
+			minutes = minutes < 10 ? '0' + minutes : minutes;
+			seconds = seconds < 10 ? '0' + seconds : seconds;
+	
+			const timerText = minutes + ':' + seconds;
+			barItem.text = `Time left: ${timerText}`;
+	
+			if (--currentTime < 0) {
+				isOver = true;
+				currentTime = duration;
+				clearInterval(timer);
+				vscode.window.showInformationMessage('Your time is over! Click on the time to resume the timer');
+			}	
+		}
+	}, 1000);
+}
+
 function activate(context) {
-	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-	let timer;
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.startTimer', function () {
+	vscode.commands.registerCommand('extension.pauseTimer', () => {
+		if (isOver) {
+			isOver = false;
+			startTimer(value, statusBarItem);
+		} else {
+			isPaused = !isPaused;
+			const message = isPaused ? 'Timer paused' : 'Timer resumed'
+			vscode.window.showInformationMessage(message);
+		}
+	});
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+	let disposable = vscode.commands.registerCommand('extension.startTimer', () => {
 		// The code you place here will be executed every time your command is executed
+		statusBarItem.command = 'extension.pauseTimer';
 		statusBarItem.show();
 
 		vscode.window.showInputBox({
@@ -19,43 +54,18 @@ function activate(context) {
 			prompt: 'Enter minutes',
 			value: '30',
 			ignoreFocusOut: true,
-		}).then(value=> {
-			const minimumNumber = parseInt(value);
+		}).then(enteredValue => {
+			const minimumNumber = parseInt(enteredValue);
 			if (isNaN(minimumNumber)) {
 				vscode.window.showErrorMessage('You must insert a number');
 				return;
 			}
-			const duration = 60 * value;
-			let currentTime = duration;
-			let minutes, seconds;
-			
-			if(timer) clearInterval(timer);
-
-			timer = setInterval(function () {
-				minutes = parseInt(currentTime / 60, 10)
-				seconds = parseInt(currentTime % 60, 10);
-		
-				minutes = minutes < 10 ? '0' + minutes : minutes;
-				seconds = seconds < 10 ? '0' + seconds : seconds;
-		
-				const timerText = minutes + ':' + seconds;
-				statusBarItem.text = `Time left: ${timerText}`;
-		
-				if (--currentTime < 0) {
-					currentTime = duration;
-					clearInterval(timer);
-					vscode.window.showInformationMessage('Your time is over!')
-						.then(() => statusBarItem.hide());
-				}
-			}, 1000);
+			value = enteredValue;
+			isOver = false;
+			startTimer(value, statusBarItem);
 		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
